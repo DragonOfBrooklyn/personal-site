07977dab9ed8a9a3bbfa4bcf21135a7554104bd4
DragonOfBrooklyn <jordan.long@useeverest.com>
2024-03-27 14:14:24 -0400

    Functional server and Anthropic connection

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,49 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios, { AxiosError } from 'axios'
 
+interface Turn {
+  role: 'user' | 'assistant',
+  content: string
+}
+
+const claudeCall = async (newQuery: string, oldConversation: Turn[], setError) => {
+  const config = {
+    headers: {
+      'jlong-authorization': 'PersonalSiteForJordanLong',
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    }
+  };
+
+  const conversation: Turn[] = [
+    ...oldConversation,
+    { role: 'user', content: newQuery }
+  ];
+
+  const claude_Res = await axios.post(
+    import.meta.env.VITE_SERVER_URL, 
+    conversation, 
+    config
+  ).catch((err: AxiosError) => {
+    console.log(err.message)
+    setError(err.message);
+  });
+  console.log(claude_Res);
+  return claude_Res;
+}
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const [error, setError] = useState(null);
+  // const [claudeResponse, setClaudeResponse] = useState('');
+  // setClaudeResponse(claudeCall());
+  const priorConvo: Turn[] = [
+    {role: 'user', content:'Who made you?'},
+    {role: 'assistant', content:'Jordan Long made me'},
+  ]
+  claudeCall("What are Jordan Long's hobbies?", priorConvo, setError);
+  // console.log('claude response', claudeCall(setError));
   return (
     <>
       <div>
@@ -22,7 +61,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Output from call {error ?? count}
         </p>
       </div>
       <p className="read-the-docs">
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: "/home/jlong09/personal-site/src/main.tsx"
+    }
+  }
 })
