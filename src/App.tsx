cce58cd0bff4a1684664bc67a17f9f4116ddb889
DragonOfBrooklyn <jordan.long@useeverest.com>
2024-04-26 11:50:41 -0400

    basic styling and page load handling

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,13 @@ import {
   TypingIndicator,
 } from "@chatscope/chat-ui-kit-react";
 import axios, { AxiosError } from 'axios';
+import { 
+  Backdrop,
+  // Box, 
+  CircularProgress, 
+  Modal, 
+  // Typography,
+} from '@mui/material';
 import JordanHiResIcon from './assets/JordanHiResIcon.jpeg';
 import './App.css'
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
@@ -33,17 +40,54 @@ const claudeCall = async (
   };
 
   await axios.post(
-    // import.meta.env.VITE_DEV_SERVER_URL, //bun dev server
-    import.meta.env.VITE_SERVER_URL, //live lambda function
+    import.meta.env.DEV
+      ? import.meta.env.VITE_DEV_SERVER_URL
+      : import.meta.env.VITE_SERVER_URL,
     {oldConversation, query}, 
     config)
   .then((value) => {
     setClaudeResponse(value.data.text);
-    // console.log(`claude responds ${new Date().toLocaleTimeString()}`);
   }).catch((err: AxiosError) => {
     console.log(err.message)
     setClaudeResponse(err.message);
   });
+}
+
+// const modalBoxStyle = {
+//   position: 'absolute' as 'absolute',
+//   top: '55vh',
+//   left: '50%',
+//   transform: 'translate(-50%, -50%)',
+//   width: '40vw',
+//   minWidth: '200px',
+//   bgcolor: '#c6e3fa',
+//   border: '2px solid #000',
+//   borderRaduis: '.7em',
+//   boxShadow: 24,
+//   p: 4,
+// };
+
+function GradientCircularProgress() {
+  return (
+    <React.Fragment>
+      <svg width={0} height={0}>
+        <defs>
+          <linearGradient id="my_gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+            <stop offset="0%" stopColor="#e01cd5" />
+            <stop offset="100%" stopColor="#1CB5E0" />
+          </linearGradient>
+        </defs>
+      </svg>
+      <CircularProgress 
+        size={'10vw'}
+        sx={{ 
+          'svg circle': { stroke: 'url(#my_gradient)' },
+          position: 'absolute',
+          top: '45vh',
+          left: '45vw',
+           }} />
+    </React.Fragment>
+  );
 }
 
 function App() {
@@ -52,6 +96,7 @@ function App() {
   const [messages, setMessages] = useState([]);
   const [claudeResponse, setClaudeResponse] = useState(undefined);
   const [claudeThinking, setClaudeThinking] = useState(false);
+  const [loading, setLoading] = useState(true);
   const conversation = React.useRef<Turn[]>([
   ]);
 
@@ -64,65 +109,82 @@ function App() {
       direction: 'outgoing' }]);
     setMsgInputValue("");
     inputRef.current?.focus();
-    claudeCall(message, conversation.current, setClaudeResponse)
+    claudeCall(message, conversation.current, setClaudeResponse);
+    conversation.current.push({role: 'user', content: message});
   };
   //on page load, make first, silent prompt to Claude
   useEffect(() => {
-    // console.log(`page load silent call ${new Date().toLocaleTimeString()}`);
-    claudeCall('Who and what are you?', conversation.current, setClaudeResponse)
+    claudeCall('What are you?', conversation.current, setClaudeResponse);
   }, []);
-
   //handle claude response
   useEffect(() => {
     if(typeof claudeResponse !== 'undefined'){
+      if(loading) setLoading(false);
       setMessages([...messages,{ 
         message: claudeResponse, 
         position: 'single',
         sender: 'Jordan AI',
         direction: 'incoming' }]);
+      conversation.current.push({role: 'assistant', content: claudeResponse});
     }
     setClaudeThinking(false);
     setClaudeResponse(undefined);
-  }, [claudeResponse])
-
+  }, [claudeResponse]);
   return (
-    <div style={{ position: "relative", height: "500px", width: "800px" }}>
-      <ChatContainer>
-        <ConversationHeader>
-          <Avatar
-            name="Jordan AI ChatBot"
-            src={JordanHiResIcon}
-          />
-          <ConversationHeader.Content
-            info="Generative AI ChatBot About Jordan Long"
-            userName="Jordan AI"
-          />
-          <ConversationHeader.Actions>
-            <InfoButton onClick={() => window.open('https://linkedin.com/in/jlongtlw', '_blank')}/>
-          </ConversationHeader.Actions>
-        </ConversationHeader>
-        <MessageList 
-          scrollBehavior="smooth" 
-          typingIndicator={
-            claudeThinking ?
-            <TypingIndicator content="Jordan AI is typing" /> :
-            <></>
-          }
-        >
-          {messages.map( (m,i) => <Message key={i} model={m} /> )}
+    <>
+      <Modal 
+        open={loading}
+        closeAfterTransition
+        slots={{backdrop: Backdrop}}
+        slotProps={{backdrop: {timeout: 500}}}
+      >
+        <>
+          {/* <Box sx={modalBoxStyle}>
 
-        </MessageList>
-        <MessageInput 
-          placeholder="Ask about Jordan..." 
-          onSend={handleSend} 
-          onChange={setMsgInputValue} 
-          value={msgInputValue} 
-          ref={inputRef}
-          attachButton={false}
-        />
+          </Box> */}
+          <GradientCircularProgress />
+        </>
+      </Modal>
+      <div 
+        className="wrapper" 
+      >
+        <ChatContainer>
+          <ConversationHeader>
+            <Avatar
+              name="Jordan AI ChatBot"
+              src={JordanHiResIcon}
+            />
+            <ConversationHeader.Content
+              info="Generative AI ChatBot About Jordan Long"
+              userName="Jordan AI"
+            />
+            <ConversationHeader.Actions>
+              <InfoButton onClick={() => window.open('https://linkedin.com/in/jlongtlw', '_blank')}/>
+            </ConversationHeader.Actions>
+          </ConversationHeader>
+          <MessageList 
+            scrollBehavior="smooth" 
+            typingIndicator={
+              claudeThinking ?
+              <TypingIndicator content="Jordan AI is typing" /> :
+              <></>
+            }
+          >
+            {messages.map( (m,i) => <Message key={i} model={m} /> )}
 
-      </ChatContainer>
-    </div>
+          </MessageList>
+          <MessageInput 
+            placeholder="Ask about Jordan..." 
+            onSend={handleSend} 
+            onChange={setMsgInputValue} 
+            value={msgInputValue} 
+            ref={inputRef}
+            attachButton={false}
+          />
+
+        </ChatContainer>
+      </div>
+    </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 //boilerplate code to embed the application in React's virtual DOM 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
